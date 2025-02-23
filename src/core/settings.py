605aea6df1612abb1d69cd6a8c885a337474e9df
ZhuraTown <swiglek@gmail.com>
2025-02-23 16:11:24 +0300

    refactoring pipline

--- src/core/settings.py
+++ src/core/settings.py
@@ -65,7 +65,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
-    "default": env.db("DATABASE_URL"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': env('DB_NAME', default=''),
+        'USER': env('DB_USER', default='postgres'),
+        'PASSWORD': env('DB_PASSWORD', default=''),
+        'HOST': env('DB_HOST', default='localhost'),
+        'PORT': env('DB_PORT', default=5432),
+    },
 }
 
 CLICKHOUSE_HOST = env('CLICKHOUSE_HOST', default='clickhouse')
--- src/users/use_cases/create_user_tests.py
+++ src/users/use_cases/create_user_tests.py
@@ -1,10 +1,7 @@
 import uuid
-from collections.abc import Generator
 from unittest.mock import ANY
 
 import pytest
-from clickhouse_connect.driver import Client
-from django.conf import settings
 from redis import Redis
 
 from core.redis_writer import get_events_from_redis
